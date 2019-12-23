e518867520420379a2ae94f64fc743f2b9103db4
jbahbouhi <jbahbouhi@stibits.io>
2019-12-23 11:27:57 +0100

    first commit

---
+++ src/main/java/com/myapp/sample/StibitsWebApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StibitsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StibitsWebApplication.class, args);
+	}
+
+}
